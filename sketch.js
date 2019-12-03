90fa41d98a20edf59ed2a3ea3b690ec4761938e4
Thero6th <48805682+Thero6th@users.noreply.github.com>
2019-12-03 14:50:39 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+/*
+p5+arduino sketch
+11.12.2019
+*/
+
+function setup() {
+    createCanvas(640, 360);
+
+    serial = new p5.SerialPort();
+    serial.on('connected', serverConnected);
+    serial.on('open', portOpen);
+    serial.on('data', serialEvent);
+    serial.on('error', serialError);
+    serial.on('close', portClose);
+
+    serial.open(portName);
+}
+
+function serverConnected() {
+    console.log('connected to server');
+}
+
+function portOpen() {
+    console.log('the serial port opened');
+}
+
+function portClose() {
+    console.log('the serial port closed');
+}
+
+function serialError() {
+    console.log("error");
+}
+
+function serialEvent() {
+    var currentString = serial.readLine();
+    trim(currentString);
+    if (!currentString) {
+        return;
+    }
+    sensorValue = currentString;
+}
+
+function draw() {
+}
