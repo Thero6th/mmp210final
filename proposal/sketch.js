870a6ba2028bc7d4ff9877d8b2e74b42decfa34f
Thero6th <48805682+Thero6th@users.noreply.github.com>
2019-12-03 14:58:38 -0500

    Add files via upload

--- proposal/sketch.js
+++ proposal/sketch.js
@@ -15,10 +15,10 @@ var prevBtnY = 500;
 var prevBtnW = 100;
 var prevBtnH = 50;
 
-var cat;
+var accel;
 
 function preload() {
-    cat = loadImage("cat.jpg");
+    accelerometer = loadImage("accel.jpg");
 }
 
 function setup() {
@@ -27,43 +27,43 @@ function setup() {
 
 function draw() {
     background(51);
- 
+
     if (slideNumber == 0) {
         // content for slide number 0
         fill(255);
         textSize(50);
         textAlign(CENTER, CENTER);
-        text("Final Project Pitch", width/2, height/2);
-        
+        text("Final Project Pitch", width / 2, height / 2);
+
         textSize(30);
-        text("by Owen", width/2, height/2 + 50);
-        
+        text("by Owen", width / 2, height / 2 + 50);
+
     } else if (slideNumber == 1) {
         // content for slide number 1
-        
+
         fill(255);
         textSize(50);
         textAlign(CENTER, CENTER);
-        text("FSR", width/2, 200);
-        
+        text("FSR", width / 2, 200);
+
         ellipse(200, 400, 100);
         rect(200, 375, 200, 50);
         fill(0);
         ellipse(200, 400, 80);
-        
+
         textSize(20);
-        text("The user hits the FSR to eject the cats from the litter box.", width/2, height - 100);
-    
+        text("The user hits the FSR to eject the cats from the litter box.", width / 2, height - 100);
+
     } else if (slideNumber == 2) {
-        
+
         // theme
-        image(cat, 0, 0, width, height);
-        text("Theme", width/2, height/2);
+        image(accel, 0, 0, width, height);
+        text("Theme", width / 2, height / 2);
     }
-    
-    
+
+
     /* drawing buttons */
-    
+
     // next btn
     if (slideNumber < totalSlides - 1) {
         fill(255);
@@ -74,36 +74,36 @@ function draw() {
         textAlign(CENTER, CENTER);
         text("Next", nextBtnX, nextBtnY);
     }
-    
-    
+
+
     // prev btn
     if (slideNumber > 0) {
-       fill(255);
+        fill(255);
         noStroke();
         rect(prevBtnX, prevBtnY, prevBtnW, prevBtnH);
         fill(0);
         textSize(20);
         textAlign(LEFT, TOP);
-        text("Previous", prevBtnX + 10, prevBtnY + 10);  
+        text("Previous", prevBtnX + 10, prevBtnY + 10);
     }
-   
-    
+
+
 }
 
 function mousePressed() {
-    
+
     // next btn
     var d = dist(mouseX, mouseY, nextBtnX, nextBtnY);
-    if (d < nextBtnSize/2 && slideNumber < totalSlides - 1) {
-        slideNumber++;    
+    if (d < nextBtnSize / 2 && slideNumber < totalSlides - 1) {
+        slideNumber++;
     }
-    
+
     // prev btn
     if (mouseX > prevBtnX &&
-       mouseX < prevBtnX + prevBtnW &&
-       mouseY > prevBtnY &&
-       mouseY < prevBtnY + prevBtnH &&
-       slideNumber > 0) {
-        slideNumber--;    
+        mouseX < prevBtnX + prevBtnW &&
+        mouseY > prevBtnY &&
+        mouseY < prevBtnY + prevBtnH &&
+        slideNumber > 0) {
+        slideNumber--;
     }
 }
